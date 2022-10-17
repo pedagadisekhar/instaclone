ba3973874d4284decf3d434cb2ee872ad0c8bd36
pedagadi chandra sekhar <pedagadisekhar@gmail.com>
2022-10-17 23:54:51 +0530

    fifth

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const app = express()
 const route = require("./src/routes/routes")
 const cors = require("cors")
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000
 
 app.use(cors())
 app.use("/",route)
